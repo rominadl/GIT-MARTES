624825926944a2665684d17e13f896f458612cbb
Romina De Laurentiis <romidelaurentiis@gmail.com>
2021-08-25 13:51:11 -0300

    Logica: Menu Hamburguesa

---
+++ scripts/master.js
@@ -0,0 +1,4 @@
+menuBtn = document.querySelector('nav .btn')
+menuLst = document.querySelector('nav .menu')
+menuBtn.onclick = () => menuLst.classList.toggle('active') ? menuBtn.
+innerHTML = '&times;' : menuBtn.innerHTML = '&equiv;'
